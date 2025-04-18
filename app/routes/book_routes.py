b8fa679bb704a2a06da142e85aff7ad93848ce1c
Max <imax.mr@gmail.com>
2025-04-18 14:58:33 -0700

    implemented error handling!

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
@@ -25,12 +25,33 @@ def get_all_books():
 # Enter the url parameter in the function arguments
 def get_one_book(book_id):
 
-    # Parameters are read as strings, so convert to int
-    book_id = int(book_id)
+    found_book = validate_book(book_id)
+
+    book = {
+        "id": found_book.id,
+        "title": found_book.title,
+        "description": found_book.description
+    }
+
+    return book
+
+# vvvvvvvvvvvvvvvvv HELPER FUNCTIONS vvvvvvvvvvvvvvvvv
+
+
+def validate_book(book_id):
+
+    # Parameters are read as strings, so try to convert to int
+    try:
+        book_id = int(book_id)
+    except:
+        # If the string can't be converted, returns a status 400 message
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
+
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description,
-            }
+            return book
+
+    # If no matching book is found, return 404 error
+    response = {"message": f"book {book_id} not found"}
+    abort(make_response(response, 404))
