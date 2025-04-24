f074e3cae06aecba7a409d39d098ee70801c9028
Max <imax.mr@gmail.com>
2025-04-24 09:26:03 -0700

    set up db migrations

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,57 +1,59 @@
 from flask import Blueprint, abort, make_response
-from app.models.book import books
+# from app.models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 
-@books_bp.get("")
-def get_all_books():
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return books_response
+# vvvvvvvvvvvvvv old code vvvvvvvvvvvvvv
 
-# No new import statements...
-# No modifications to the other route...
+# @books_bp.get("")
+# def get_all_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append({
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description
+#         })
+#     return books_response
 
-# Surround url parameter with arrow brackets to designate it as a passed variable
+# # No new import statements...
+# # No modifications to the other route...
 
-
-@books_bp.get("/<book_id>")
-# Enter the url parameter in the function arguments
-def get_one_book(book_id):
-
-    found_book = validate_book(book_id)
-
-    book = {
-        "id": found_book.id,
-        "title": found_book.title,
-        "description": found_book.description
-    }
-
-    return book
-
-# vvvvvvvvvvvvvvvvv HELPER FUNCTIONS vvvvvvvvvvvvvvvvv
+# # Surround url parameter with arrow brackets to designate it as a passed variable
 
 
-def validate_book(book_id):
+# @books_bp.get("/<book_id>")
+# # Enter the url parameter in the function arguments
+# def get_one_book(book_id):
 
-    # Parameters are read as strings, so try to convert to int
-    try:
-        book_id = int(book_id)
-    except:
-        # If the string can't be converted, returns a status 400 message
-        response = {"message": f"book {book_id} invalid"}
-        abort(make_response(response, 400))
+#     found_book = validate_book(book_id)
 
-    for book in books:
-        if book.id == book_id:
-            return book
+#     book = {
+#         "id": found_book.id,
+#         "title": found_book.title,
+#         "description": found_book.description
+#     }
 
-    # If no matching book is found, return 404 error
-    response = {"message": f"book {book_id} not found"}
-    abort(make_response(response, 404))
+#     return book
+
+# # vvvvvvvvvvvvvvvvv HELPER FUNCTIONS vvvvvvvvvvvvvvvvv
+
+
+# def validate_book(book_id):
+
+#     # Parameters are read as strings, so try to convert to int
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         # If the string can't be converted, returns a status 400 message
+#         response = {"message": f"book {book_id} invalid"}
+#         abort(make_response(response, 400))
+
+#     for book in books:
+#         if book.id == book_id:
+#             return book
+
+#     # If no matching book is found, return 404 error
+#     response = {"message": f"book {book_id} not found"}
+#     abort(make_response(response, 404))
