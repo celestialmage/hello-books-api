d236ba86b8c83403dd797f6466750f5763026fe5
Max <imax.mr@gmail.com>
2025-04-29 22:01:24 -0700

    updated gitignore and app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,24 @@
 from flask import Flask
 from .db import db, migrate
-from .models import book # Newly added import
+from .models import book
 from .routes.book_routes import books_bp
-# from .routes.hello_world_routes import hello_world_bp
+import os
 
-
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    if config:
+        # Merge `config` into the app's configuration
+        # to override the app's default settings
+        app.config.update(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # register blueprints here
-    # app.register_blueprint(hello_world_bp)
+    # Register Blueprints here
     app.register_blueprint(books_bp)
 
     return app
